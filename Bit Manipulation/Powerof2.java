594e061366f31a4e039c72e460fe7475ddd6367c
Priyanshu Gupta <priyanshusgbm05@gmail.com>
2025-11-08 19:29:52 +0530

    Fourth Day

---
+++ Bit Manipulation/Powerof2.java
@@ -0,0 +1,9 @@
+// Check if a number is a Power of 2 or not
+public class Powerof2 {
+    public static boolean isPowerOfTwo(int n){
+        return (n & (n-1)) == 0;
+    }
+    public static void main(String[] args) {
+        System.out.println(isPowerOfTwo(15));
+    }
+}
