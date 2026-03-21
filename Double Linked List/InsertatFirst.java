1340cd8f45d613209284fe659dcd40af8932e21c
Priyanshu Gupta <priyanshusgbm05@gmail.com>
2026-03-21 20:29:14 +0530

    Linkded List

--- Double Linked List/InsertatFirst.java
+++ Double Linked List/InsertatFirst.java
@@ -1,4 +1,3 @@
-import java.util.*;
 
 class Node{
     int data;
@@ -20,16 +19,67 @@ class Node{
 
 class Solution{
     //Function to convert an array to doubly Linked list
-
     public static Node convertArr2DLL(int[] arr){
+    //create the head node with the first element of the array
     Node head = new Node(arr[0]);
-    Node prev = head;
+    Node prev = head; //Intialize 'prev' to the head node
 
-    for (int i = 1 ; i<arr/.length ; i++){
-        
+    //Taverse the array to create the doubly linked list
+    for (int i = 1 ; i<arr.length ; i++){
+        Node temp = new Node(arr[i], null, prev);  // Create a new node
+            prev.next = temp;  // Set 'next' of the previous node to the new node
+            prev = temp;  // Move 'prev' to the new node
+        }
+
+        return head;  // Return the head of the doubly linked list
     }
+
+    // Function to print the elements of the doubly linked list
+    public static void print(Node head) {
+        // Traverse through the list and print each node's data
+        while (head != null) {
+            System.out.print(head.data + " ");  // Print the data of the current node
+            head = head.next;  // Move to the next node
+        }
+        System.out.println();  // New line after printing the list
     }
+
+    // Function to insert a new node at the tail of the doubly linked list
+   public static Node insertAtTail(Node head, int k) {
+    Node newNode = new Node(k);
+
+    if (head == null) {
+        return newNode;
+    }
+
+    Node tail = head;
+    while (tail.next != null) {
+        tail = tail.next;
+    }
+
+    tail.next = newNode;
+    newNode.prev = tail;   // FIXED
+
+    return head;
+}
 }
 public class InsertatFirst {
-    
+    public static void main(String[] args) {
+        // Intialize an array of Integers
+        int [] arr = {12 , 5 ,8,7,4};
+
+        //convert the array to a doubly Linked list
+        Node head = Solution.convertArr2DLL(arr);
+
+        //Print the initially created doubly linked list
+        System.out.println("Doubly Linked List Initially: ");
+        Solution.print(head);
+
+        //Insert a node with value 10 at the end of the doubly Linked List
+        System.out.println("\n Doubly Linked List After inserting at the tail with value 10:");
+        head = Solution.insertAtTail(head , 10);
+        Solution.print(head);
+        
+
+    }
 }
